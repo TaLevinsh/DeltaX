0e443c3ead2669b8a6243e3a63c95efc66b6eefc
Tal Levinshtein <39327757+TaLevinsh@users.noreply.github.com>
2025-01-26 18:38:33 +0200

    Update DeltaX.py

--- DeltaX.py
+++ DeltaX.py
@@ -3,175 +3,153 @@ from datetime import *
 import re
 import sys
 
+# Constants for registry paths
 MOUNTED_DEVICES = 'System\MountedDevices'
 USB = 'System\CurrentControlSet\Enum\USB'
-CONFIG = 'C:\Users\Owner\PycharmProjects\Git\DeltaX\Config.txt'     # What keys to scan
+CONFIG_PATH = r'C:\Users\Owner\PycharmProjects\Git\DeltaX\Config.txt'  # Config file path
 
+def read_config():
+    """Reads registry keys to scan from the config file."""
+    with open(CONFIG_PATH, 'r') as file:
+        return [line.strip() for line in file.readlines()]
 
-def Read_Config():
-    F = open(CONFIG, 'r')
-    KeyNames = F.read().split('\n')
-    return KeyNames
+def translate_reg_time(handle):
+    """Translates registry timestamp to human-readable datetime."""
+    reg_time = QueryInfoKey(handle)[2]
+    epoch = datetime(1601, 1, 1)
+    return epoch + timedelta(microseconds=reg_time / 10)
 
-
-def Translate_Reg_Time(Handle):
-    RegTime = QueryInfoKey(Handle)[2]
-    Epoch = datetime(1601, 1, 1)
-    Calculated = Epoch + timedelta(microseconds=RegTime / 10)
-    return Calculated
-
-
-def Read_Key(Key):
-    Results = {}
-    Count = 0
-    Handle = OpenKey(HKEY_LOCAL_MACHINE, Key)
+def read_registry_key(key):
+    """Reads values from the given registry key."""
+    results = {}
     try:
-        I = 0
+        handle = OpenKey(HKEY_LOCAL_MACHINE, key)
+        i = 0
         while True:
-            Value = EnumValue(Handle, I)
-            Results[Value[0]] = Value[1]
-            I = I + 1
+            value = EnumValue(handle, i)
+            results[value[0]] = value[1]
+            i += 1
     except WindowsError:
         pass
-    Change_Time = Translate_Reg_Time(Handle)
-    return Results, Change_Time
+    return results, translate_reg_time(handle)
 
+def update_file(current_state, file_name):
+    """Updates the baseline file with current registry values."""
+    with open(file_name, 'w') as file:
+        for value in current_state.values():
+            file.write(f"{value}\n")
 
-def Update_File(Current_State, FileName):
-    Current_Values = Current_State.values()
-    F = open(FileName, 'w')
-    for V in Current_Values:
-        F.write(V + '\n')
-    F.close()
+def read_file(file_name):
+    """Reads the content of the baseline file."""
+    with open(file_name, 'r') as file:
+        return [line.strip() for line in file.readlines()]
 
-
-def Read_File(FileName):
-    F = open(FileName, 'r')
-    Results = F.read().split('\n')
-    F.close()
-    return Results
-
-
-def Show_Key_Delta():
-    KeyNames = Read_Config()
-    KeyName = KeyNames[0]
-    print 'Found', len(KeyNames), 'keys to scan, type "Y" to apply.'
-    for I, K in enumerate(KeyNames):
-        Ans = raw_input('[{}] '.format(I + 1) + K + ' >  ')
-        if Ans == 'Y':
-            KeyName = K
+def show_key_delta():
+    """Displays the delta between current and baseline registry values."""
+    key_names = read_config()
+    print(f"Found {len(key_names)} keys to scan. Type 'Y' to apply.")
+    for i, key in enumerate(key_names):
+        ans = input(f"[{i + 1}] {key} > ")
+        if ans.upper() == 'Y':
+            selected_key = key
             break
-    FileName = 'C:\\Users\\Owner\\PycharmProjects\\Git\\DeltaX\\' + raw_input('Insert baseline file name > ')   # BaseLine.txt
-    Baseline = Read_File(FileName)
-    Current, ChangeTime = Read_Key(KeyName)
-    New = 0
-    print
-    for C in Current.values():
-        if C in Baseline:
-            print '(Exists)', C
-        else:
-            New += 1
-            print '@ NEW VALUE @', C
-    if New > 0:
-        print '\n', New, 'New values have been added.'
-        A = raw_input('Would you like to update the baseline file? [Y | N]'
-                      '\n> ')
-        if A == 'Y':
-            Update_File(Current, FileName)
-            print 'Baseline updated with', New, 'new entries.'
-            print 'Change time:', ChangeTime
+    baseline_file = input("Insert baseline file name > ")
+    baseline = read_file(baseline_file)
+    
+    current, change_time = read_registry_key(selected_key)
+    new_values = [value for value in current.values() if value not in baseline]
+
+    if new_values:
+        print(f"\n{len(new_values)} new values have been added.")
+        for value in new_values:
+            print(f"@ NEW VALUE @ {value}")
+
+        update = input(f"Would you like to update the baseline file? [Y/N] > ")
+        if update.upper() == 'Y':
+            update_file(current, baseline_file)
+            print(f"Baseline updated with {len(new_values)} new entries.")
+            print(f"Change time: {change_time}")
     else:
-        print '\nNo new entries were found - Current data matches baseline.'
-        print 'Change time:', ChangeTime
+        print("\nNo new entries found - Current data matches baseline.")
+        print(f"Change time: {change_time}")
 
+def key_delta_quick():
+    """Quick scan of the 'Run' registry key for new entries."""
+    key_name = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
+    baseline_file = r"C:\DeltaX\Registry\Run.txt"
+    baseline = read_file(baseline_file)
+    
+    current, change_time = read_registry_key(key_name)
+    new_values = [value for value in current.values() if value not in baseline]
 
-def Key_Delta_Quick():
-    KeyName = 'SOFTWARE\Microsoft\Windows\CurrentVersion\Run'
-    FileName = 'C:\\DeltaX\\Registry\\Run.txt'
-    Baseline = Read_File(FileName)
-    Current, ChangeTime = Read_Key(KeyName)
-    New = 0
-    print
-    for C in Current.values():
-        if C in Baseline:
-            print '(exists)', C
-        else:
-            New += 1
-            print '@ NEW VALUE @', C
-    if New > 0:
-        print '\n', New, 'New values have been added.'
+    if new_values:
+        print(f"\n{len(new_values)} new values have been added.")
+        for value in new_values:
+            print(f"@ NEW VALUE @ {value}")
 
-        A = raw_input('Would you like to update the baseline file? [Y | N]'
-                      '\n> ')
-        if A == 'Y':
-            Update_File(Current, FileName)
-            print 'Baseline updated with', New, 'new entries.'
-            print 'Change time:', ChangeTime
+        update = input(f"Would you like to update the baseline file? [Y/N] > ")
+        if update.upper() == 'Y':
+            update_file(current, baseline_file)
+            print(f"Baseline updated with {len(new_values)} new entries.")
+            print(f"Change time: {change_time}")
     else:
-        print '\nNo new entries were found - Current data matches baseline.'
-        print 'Change time:', ChangeTime
+        print("\nNo new entries found - Current data matches baseline.")
+        print(f"Change time: {change_time}")
 
+def analyze_key():
+    """Analyze connected USB devices and display relevant information."""
+    temp = "VID_(.+)&PID_(.+)"
+    results = []
+    vendors = {}
+    devices = 0
 
-def Analyze_Key():
-    Temp = "VID_(.+)&PID_(.+)"
-    Results = []
-    Devices = 0
-    Vendors = {}
-    Handle = OpenKey(HKEY_LOCAL_MACHINE, USB)
     try:
-        I = 0
+        handle = OpenKey(HKEY_LOCAL_MACHINE, USB)
+        i = 0
         while True:
-            KeyName = EnumKey(Handle, I)
-            Results.append(KeyName)
-            if re.match('VID*', KeyName):
-                Devices += 1
-                VidPid = re.search(Temp, KeyName)
-                Vid, Pid = VidPid.group(1), VidPid.group(2)
-                if Vid in Vendors.keys():
-                    Vendors[Vid] += 1
-                else:
-                    Vendors[Vid] = 1
-            I += 1
+            key_name = EnumKey(handle, i)
+            results.append(key_name)
+            if re.match(r'VID*', key_name):
+                devices += 1
+                vid_pid = re.search(temp, key_name)
+                vid, pid = vid_pid.group(1), vid_pid.group(2)
+                vendors[vid] = vendors.get(vid, 0) + 1
+            i += 1
     except WindowsError:
         pass
-    print 'List'
-    for i in Results:
-        print i
-    print '\nTotal of', Devices, 'devices were connected to this host.'
-    print '\n\nVendors list'
 
-    for V in Vendors:
-        print V, ':', Vendors[V], 'devices'
+    print("Devices connected:")
+    for result in results:
+        print(result)
+    print(f"\nTotal of {devices} devices were connected to this host.")
+    
+    print("\nVendor list:")
+    for vendor, count in vendors.items():
+        print(f"{vendor}: {count} devices")
 
-    whole_keytime = Translate_Reg_Time(Handle)
-    print 'Last device connection time:', whole_keytime
-    print datetime.now() - whole_keytime, 'ago'
-    for R in Results:
-        K = OpenKey(HKEY_LOCAL_MACHINE, USB + "\\" + R)
-        Keytime = Translate_Reg_Time(K)
-        print R, '\nchanged at', Keytime
+    last_device_time = translate_reg_time(handle)
+    print(f"Last device connection time: {last_device_time}")
+    print(f"{datetime.now() - last_device_time} ago")
 
+    for result in results:
+        key = OpenKey(HKEY_LOCAL_MACHINE, USB + "\\" + result)
+        key_time = translate_reg_time(key)
+        print(f"{result}\nChanged at {key_time}")
 
 def main():
-    if len(sys.argv) > 1:
-        if sys.argv[1] == 'Quickscan':
-            Key_Delta_Quick()
+    """Main function to drive the DeltaX tool."""
+    if len(sys.argv) > 1 and sys.argv[1].lower() == 'quickscan':
+        key_delta_quick()
     else:
-        print '- - - Delta X - - -'
-
-        print 'Please select action:'
-
-        Opt = input('[1] Analyze Connected Devices\n'
-                    '[2] Show Key Delta\n'
-                    '>\t')
-
-        if Opt == 1:
-            Analyze_Key()
-        if Opt == 2:
-            Show_Key_Delta()
+        print('- - - Delta X - - -')
+        print('Please select an action:')
+        opt = input('[1] Analyze Connected Devices\n[2] Show Key Delta\n> ')
+        if opt == '1':
+            analyze_key()
+        elif opt == '2':
+            show_key_delta()
         else:
-            print 'Invalid Selection!'
-
+            print('Invalid Selection!')
 
 if __name__ == '__main__':
     main()
